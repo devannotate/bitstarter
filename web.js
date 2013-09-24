d3bfaac37a65c0fe703f921bcda25054a9367521
devannotate <dev.annotate@gmail.com>
2013-09-24 02:50:27 +0000

    Reading from the file index.html

--- web.js
+++ web.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var app = express();
+// Start Addign the code  for reading from the file
+var fs = require('fs');
+// End Addig the code
 app.use(express.logger());
 
+
+// Start Addign the code  for reading from the file
+var readString = fs.readFileSync('index.html');
+// End Addig the code
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ // Commenting the Existing Code
+ // response.send('Hello World 2!');
+  
+// Start Addign the code  for reading from the file
+  response.send(readString.toString('utf-8'));
+// End Addig the code
 });
 
 var port = process.env.PORT || 5000;
